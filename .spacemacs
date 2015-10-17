4a7c12e0cd6e2587b57410f3b747646bc3023e8c
ryane <ryanesc@gmail.com>
2015-10-17 11:59:11 -0400

    added rae-auto-save layer

--- .spacemacs
+++ .spacemacs
@@ -38,6 +38,7 @@ values."
      ;;
      osx
      git
+     rae-auto-save
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
---
+++ private/rae-auto-save/funcs.el
@@ -0,0 +1,52 @@
+(defcustom rae-auto-save t
+  "Non-nil values enable auto save."
+  :type 'boolean
+  :group 'rae)
+
+;; automatically save buffers associated with files on buffer switch
+;; and on windows switch
+(defun rae-auto-save-command ()
+  "Save the current buffer if `rae-auto-save' is not nil."
+  (when (and rae-auto-save
+             buffer-file-name
+             (buffer-modified-p (current-buffer))
+             (file-writable-p buffer-file-name))
+    (save-buffer)))
+
+(defmacro advise-commands (advice-name commands &rest body)
+  "Apply advice named ADVICE-NAME to multiple COMMANDS.
+   The body of the advice is in BODY."
+  `(progn
+     ,@(mapcar (lambda (command)
+                 `(defadvice ,command (before ,(intern (concat (symbol-name command) "-" advice-name)) activate)
+                    ,@body))
+               commands)))
+
+;; advise all window switching functions
+(advise-commands "auto-save"
+                 (switch-to-buffer
+                  other-window
+                  windmove-up
+                  windmove-down
+                  windmove-left
+                  windmove-right
+                  evil-window-left
+                  evil-window-right
+                  evil-window-up
+                  evil-window-down
+                  buf-move-left
+                  buf-move-right
+                  buf-move-up
+                  buf-move-down
+                  select-window
+                  select-window-1
+                  select-window-2
+                  select-window-3
+                  select-window-4
+                  select-window-5
+                  select-window-6
+                  select-window-7
+                  select-window-8
+                  select-window-9)
+                 (rae-auto-save-command))
+(add-hook 'mouse-leave-buffer-hook 'rae-auto-save-command)
