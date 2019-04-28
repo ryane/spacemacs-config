a545516f8b150c46a21248b20ed677a8f6083eea
ryane <ryanesc@gmail.com>
2019-04-28 16:26:25 -0400

    fixed org-journal

--- private/rae-org/packages.el
+++ private/rae-org/packages.el
@@ -1,8 +1,13 @@
 (defconst rae-org-packages
   '(
     org-plus-contrib
+    org-journal
     ))
 
 (defun rae-org/post-init-org-plus-contrib ()
   (require 'org-checklist)
+  )
+
+(defun rae-org/init-org-journal ()
+  ;; nothing to do
   )
